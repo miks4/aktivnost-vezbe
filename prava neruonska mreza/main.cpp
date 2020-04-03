261d9c4b2f5ebbe2c4606c3d72f6aaa67e0f9c5b
miks4 <62470347+miks4@users.noreply.github.com>
2020-04-03 18:17:18 +0200

    odradjeno

--- prava neruonska mreza/main.cpp
+++ prava neruonska mreza/main.cpp
@@ -59,14 +59,14 @@ private:
     int brOptimizacija;
     STANJE_TESTA stanje;
 public:
-    Test(int zpu,int bo,STANJE_TESTA st,int bs,int bn) : nm(bs,bn){
+    Test(int zpu,int bo,STANJE_TESTA st,int bs,int bn) : nm(3,200){
         stanje = st;
         brOptimizacija = bo;
-        if(0 > brOptimizacija && brOptimizacija > 1){
-            brOptimizacija = 0.85;
+        if(0 > zeljeniProcenatUspesnosti || zeljeniProcenatUspesnosti > 1){
+            zeljeniProcenatUspesnosti = 0.85;
         }
         else{
-            brOptimizacija = bo;
+            zeljeniProcenatUspesnosti = zpu;
         }
     }
     STANJE_TESTA get_stanje()const{
@@ -87,10 +87,117 @@ public:
     TIP_MREZE get_tiip()const{
         return nm.get_tip();
     }
+    bool procesiraj(){
+        if((stanje == START || stanje == OPTIMIZACIJA) && zeljeniProcenatUspesnosti > nm.Izracunaj_uspeh()){
+            stanje = PROCESIRANJE;
+            return true;
+        }
+        if((stanje == START || stanje == OPTIMIZACIJA) && zeljeniProcenatUspesnosti <= nm.Izracunaj_uspeh()){
+            stanje = KRAJ;
+            cout<<"broj optimizacija:"<<brOptimizacija<<endl;
+            cout<<"zeljena uspesnost:"<<zeljeniProcenatUspesnosti<<endl;
+            cout<<"tacnost:"<<nm.Izracunaj_uspeh()<<endl;
+            cout<<"uspesno izvrsen!"<<endl;
+            return true;
+        }
+        return false;
+    }
+    bool potrebnoOptimizovati(){
+        if(stanje == PROCESIRANJE){
+            stanje = OPTIMIZACIJA;
+            return true;
+        }
+        return false;
+    }
+    bool optimizuj(){
+        if(stanje == OPTIMIZACIJA){
+            nm.set_neuroniPoSloju(nm.get_neuroniPoSloju()+1);
+        nm.set_skrivenisloj(nm.get_skriveniSloj()+1);
+            return true;
+        }
+    }
+    double izracunajUspeh(){
+        return nm.Izracunaj_uspeh();
+    }
 
 };
+void ispisiTest(const Test &t){
+    cout<<"stanje testa:";
+    switch(t.get_stanje()){
+    case 1:
+        cout<<"start"<<endl;
+        break;
+    case 2:
+        cout<<"procesiranje"<<endl;
+        break;
+    case 3:
+        cout<<"optimizacija"<<endl;
+        break;
+    case 4:
+        cout<<"kraj"<<endl;
+        break;
+    default:
+        break;
+    }
+    cout<<"tip neuronske mreze:";
+    switch(t.get_tiip()){
+    case 1:
+        cout<<"duboku"<<endl;
+        break;
+    case 2:
+        cout<<"obicni"<<endl;
+        break;
+    default:
+        break;
+    }
+    cout<<"tacnost:"<<t.izracunajUspeh()<<endl;
+
+}
+int meni(){
+    int i;
+    cout<<"1.procesiraj"<<endl;
+    cout<<"2.potrebno optimizovati"<<endl;
+    cout<<"3.optimizuj"<<endl;
+    cout<<"4.kraj"<<endl;
+    cin>>i;
+    return i;
+}
 int main()
 {
+    Test t;
+    do{
+        switch(meni()){
+        case 1:
+            if(t.procesiraj()==true){
+                cout<<"izvrseno"<<endl;
+            }
+            else{
+                cout<<"nije izvrseni";
+            }
+            break;
+        case 2:
+            if(t.potrebnoOptimizovati()==true){
+                cout<<"izvrseno"<<endl;
+            }
+            else{
+                cout<<"nije izvrseni";
+            }
+            break;
+        case 3:
+            if(t.optimizuj()==true){
+                cout<<"izvrseno"<<endl;
+            }
+            else{
+                cout<<"nije izvrseni";
+            }
+            break;
+        case 4:
+            ispisiTest(t);
+            return 0;
+        default:
+            break;
+        }
 
+    }while(1);
     return 0;
 }
